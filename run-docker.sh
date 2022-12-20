df45c743d2f654a62ba3e406f073e27efa5a85ef
thebesttv <taojuntian@outlook.com>
2022-12-20 23:20:55 +0800

    Rename REPORT_DIR to SBLT_REPORT_DIR

--- run-docker.sh
+++ run-docker.sh
@@ -2,11 +2,11 @@
 
 CONTAINER_RESULT_DIR=/home/thebesttv/Static-Bug-Detectors-ASE-Artifact/analyzers/results
 
-REPORT_DIR=sblt-proj-reports
+SBLT_REPORT_DIR=sblt-proj-reports
 
 docker run --rm -it \
        -p 127.0.0.1:50022:22 \
        -v /var/run/docker.sock:/var/run/docker.sock \
-       -v $REPORT_DIR:$CONTAINER_RESULT_DIR/$REPORT_DIR \
+       -v $SBLT_REPORT_DIR:$CONTAINER_RESULT_DIR/$SBLT_REPORT_DIR \
        thebesttv/real-world:main \
        /bin/bash
