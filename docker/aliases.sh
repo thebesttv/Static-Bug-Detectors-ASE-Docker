493c9676f93bf043c5dae5f1b4961eab1a0d3e3c
thebesttv <taojuntian@outlook.com>
2022-12-21 20:55:33 +0800

    Add useful bash aliases

---
+++ docker/aliases.sh
@@ -0,0 +1,15 @@
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+alias ll='ls -alhF'
+alias la='ls -A'
+alias l='ls -CF'
+
+alias lt='du -sh * | sort -hr'
+
+# Overwrite existing commands for better defaults
+alias mv="mv -i"           # -i prompts before overwrite
+alias mkdir="mkdir -p"     # -p make parent dirs as needed
+alias df="df -h"           # -h prints human readable format
