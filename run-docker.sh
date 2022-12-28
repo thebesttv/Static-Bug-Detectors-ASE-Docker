0aaa29b5a4e94624392c57521de1a90066435498
thebesttv <taojuntian@outlook.com>
2022-12-28 09:30:20 +0800

    Add volumes for other projects

--- run-docker.sh
+++ run-docker.sh
@@ -4,11 +4,19 @@ CONTAINER_RESULT_DIR=/home/thebesttv/Static-Bug-Detectors-ASE-Artifact/analyzers
 
 SBLT_REPORT_DIR=sblt-proj-reports
 SBHT_REPORT_DIR=sbht-proj-reports
+INFER_REPORT_DIR=infer-proj-reports
+NULLAWAY_REPORT_DIR=nullaway-proj-reports
+CF_REPORT_DIR=checkerframework-proj-reports
+ERADICATE_REPORT_DIR=eradicate-proj-reports
 
 docker run --rm -it \
        -p 127.0.0.1:50022:22 \
        -v /var/run/docker.sock:/var/run/docker.sock \
        -v $SBLT_REPORT_DIR:$CONTAINER_RESULT_DIR/$SBLT_REPORT_DIR \
        -v $SBHT_REPORT_DIR:$CONTAINER_RESULT_DIR/$SBHT_REPORT_DIR \
+       -v $INFER_REPORT_DIR:$CONTAINER_RESULT_DIR/$INFER_REPORT_DIR \
+       -v $NULLAWAY_REPORT_DIR:$CONTAINER_RESULT_DIR/$NULLAWAY_REPORT_DIR \
+       -v $CF_REPORT_DIR:$CONTAINER_RESULT_DIR/$CF_REPORT_DIR \
+       -v $ERADICATE_REPORT_DIR:$CONTAINER_RESULT_DIR/$ERADICATE_REPORT_DIR \
        thebesttv/real-world:main \
        /bin/bash
